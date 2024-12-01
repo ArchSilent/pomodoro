402d8c3bed3cbead08a5e6247c87f92c41ae95f4
Silent <didierccasanova@gmail.com>
2024-12-01 15:48:28 -0500

    Revert "first commit6"
    This reverts commit 225f62c26f85488e728d33515baeddfb43847ccc.

--- android/app/src/main/kotlin/com/example/pomotoro/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pomotoro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color backgroundColor = Color(0xFFE7626C);
-const String welcomeText = '¡Bienvenido a Pomotoro!';
